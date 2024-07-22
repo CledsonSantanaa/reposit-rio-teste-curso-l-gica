1c4a4c1dc80fcf824d2ef9935f5969e05da1ef79
Cledson De Santana <cledson.santana17@hotmail.com>
2024-07-22 20:51:29 -0300

    testando conexão

---
+++ index.js
@@ -0,0 +1,41 @@
+let idade = 18
+let vistoVerificado = true
+
+console.log((idade >= 18) && (vistoVerificado === true))
+
+
+
+// LAÇO DE REPETIÇÃO   "for".
+// for (let contador = 0; contador < 4; contador ++){
+//     console.log(contador)
+//     console.log("aumentando o contador")
+// }
+
+
+// let pontoDeVidas = 0
+
+// for(let i = 0; i < 11 ; i ++){
+//     pontoDeVidas += 1
+//     console.log("Ganho mais uma vida " + i)
+// }
+
+// console.log(pontoDeVidas + "Total ")
+
+
+
+// while / ENQUANTO
+// let contador = 0
+
+// while(contador < 3){
+//     console.log("Olá")
+//     contador++
+// }
+
+
+// do while  ... "do" = faça
+let contador = 0
+
+do {
+    console.log("olá")
+    contador++
+} while (contador < 3)
